62c219c2defc85d29a0ac0b81150276ad593d3c8
Sean Norwood <norwood.sean@gmail.com>
2022-04-03 20:03:30 +0000

    chore: add custom head element

---
+++ tubearchivist/www/src/components/CustomHead.tsx
@@ -0,0 +1,50 @@
+import Head from "next/head";
+
+export const CustomHead = ({ title }: { title: string }) => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#01202e"
+      />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="apple-mobile-web-app-title" content="TubeArchivist" />
+      <meta name="application-name" content="TubeArchivist" />
+      <meta name="msapplication-TileColor" content="#01202e" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content="#01202e" />
+      {/* {% if title %} */}
+      <title>TA | {{ title }}</title>
+      {/* {% else %} */}
+      <title>TubeArchivist</title>
+      {/* {% endif %} */}
+      {/* {% if colors == "dark" %} */}
+      {/* <link rel="stylesheet" href="/css/dark.css" /> */}
+      {/* {% else %} */}
+      {/* <link rel="stylesheet" href="/css/light.css" /> */}
+      {/* {% endif %} */}
+    </Head>
+  );
+};
