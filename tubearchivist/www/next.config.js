3870a54d2e49961fb2e7044ad2b9d9013be7f95b
Sean Norwood <norwood.sean@gmail.com>
2022-04-03 00:28:12 +0000

    chore: enable streaming ssr

--- tubearchivist/www/next.config.js
+++ tubearchivist/www/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "nodejs",
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
